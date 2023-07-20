c9650b1b29e61d373da2007cf02f41a275e60573
itinteractive <itinteractiveacademy@gmail.com>
2023-07-20 14:36:13 +0500

    getcombyid commit

--- lib/getCommentsById.ts
+++ lib/getCommentsById.ts
@@ -3,21 +3,21 @@ import prisma from "@/lib/prisma";
 import { cache } from "react";
 
 
-const getCommentsById = async (post_id) => {
-try{
-  const data = await prisma.comments.findMany({
-    where: {
-        comment_post_ID=post_id;
-    },
-  });
+const getCommentsById = async (post_id: string)  => {
+// try{
+//   const data = await prisma.comments.findMany({
+//     where: {
+//       comment_post_ID:BigInt(post_id),
+//     },
+//   });
   
   
-  return data; 
+//   return data; 
 
-  } catch (error) {
-    console.log(error);
-    return null;
-  }
+//   } catch (error) {
+//     console.log(error);
+//     return null;
+//   }
 };
 
 export default getCommentsById;
