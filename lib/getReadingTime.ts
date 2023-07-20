bd05f16e22c4445b75e3e28716f1a591e26c0242
itinteractive <itinteractiveacademy@gmail.com>
2023-07-20 17:56:35 +0500

    reading time text commit

--- lib/getReadingTime.ts
+++ lib/getReadingTime.ts
@@ -3,11 +3,13 @@
 const getReadingTime = (text: string) => {
   // return readingTime(text).text;
   const wordsPerMinute = 200; // Adjust this value based on your reading speed
-
+if (text) {
   const wordCount = text.trim().split(/\s+/).length;
   const readingTimeMinutes = Math.ceil(wordCount / wordsPerMinute);
 
   return `${readingTimeMinutes} minute${readingTimeMinutes !== 1 ? 's' : ''} read`;
+}
+ return '0 min read';
 };
 
 export default getReadingTime;
