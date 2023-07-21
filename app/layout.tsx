69e5ff05e190956d02e3a028828baa8ebe596ea4
itinteractive <itinteractiveacademy@gmail.com>
2023-07-21 08:37:05 +0500

    app error3 commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import GoogleAnalytics from "@/components/GoogleAnalytics";
 import CookieBanner from "@/components/Cookie/CookieBanner";
 import AdsComponent from "@/components/AdsComponent";
 import Script from "next/script";
+import Head from 'next/head'
 
 const siteUrl:any = process.env.NEXT_PUBLIC_DOMAIN_URL;
 const ga_id:any= process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
@@ -27,11 +28,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
+      <Head>
       <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9018422630165731"
      crossOrigin="anonymous"
      strategy="lazyOnload" />
-      </head>
+      </Head>
       <body>
       {/* <GoogleAnalytics GA_MEASUREMENT_ID={ga_id}/> */}
         <Navbar />
